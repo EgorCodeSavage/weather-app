4bf8021d1747191b9d03ff8e788101f0b09574ca
EgorSavage <119173955+EgorSavage@users.noreply.github.com>
2023-02-28 12:57:01 +0100

    Доделать картинку по погоде

--- src/components/weather_inner/WeatherInner.jsx
+++ src/components/weather_inner/WeatherInner.jsx
@@ -2,9 +2,14 @@ import "./style.scss"
 
 import locationLogo from "./../../img/location.png"
 import temperatureLogo from "./../../img/temperature.png"
+import weather05 from "./../../img/weather05.png"
 import weather02 from "./../../img/weather02.png"
+
 import searchLocation from "./../../img/search_location.png"
+import { cities } from "../../helpers/countryList"
+
 import { useEffect, useState } from "react"
+
 
 
 
@@ -13,18 +18,30 @@ const WeatherInner = () => {
 
     const myKey = "a2d6f8dc708abc3fda57ac0c1b199b66";
     const [info, setInfo] = useState([]);
+    const [weathers, setWeathers] = useState([]);
+    const [main, setMain] = useState([]);
+    const [wind, setWind] = useState([]);
+
+
 
 
     
 
     useEffect(() => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=53.429&lon=14.553&appid=${myKey}&units=metric`).then(res => res.json()).then(json => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?${cities[0].cordinates}&appid=${myKey}&units=metric`).then(res => res.json()).then(json => {
             setInfo(json)
+            setWeathers(json.weather)
+            setMain(json.main)
+            setWind(json.wind)
         }).catch(err => {
             console.log(err);
             alert("Ошибка при загрузке данных!")
         })
     }, [])
+
+    console.log(weathers);
+
+
 
     return ( 
         <div className="weather_inner_container">
@@ -36,18 +53,18 @@ const WeatherInner = () => {
                 <div className="weather_inner_location">{info.name}<img src={locationLogo} alt="location"/></div>
                 <div className="weather_inner_weather">
                     <img src={temperatureLogo} alt="temperature" />
-                    <div className="current_temp"><span>{}</span>°C</div>
-                    <img src={weather02} alt="cloudly" />
+                    <div className="current_temp"><span>{Math.round(main.temp)}</span>°C</div>
+                    <img src={weather05} alt="" />   {/*    ДОДЕЛАТЬ КАРТИНКУ ПО ПОГОДЕ     */}
                 </div>
                 <div className="weather_inner_date"></div>
                 <div className="weather_inner_stats">
                     <div className="stat01">
                         <div className="title">HUMIDITY</div>
-                        <div>%</div>
+                        <div>{main.humidity}%</div>
                     </div>
                     <div className="stat02">
                         <div className="title">VISIBILITY</div>
-                        <div>km</div>
+                        <div>{info.visibility / 1000}km</div>
                     </div>
                     <div className="stat03">
                         <div className="title">AIR PRESSURE</div>
@@ -55,7 +72,7 @@ const WeatherInner = () => {
                     </div>
                     <div className="stat04">
                         <div className="title">WIND</div>
-                        <div>mph</div>
+                        <div>{wind.speed}mph</div>
                     </div>
                 </div>
             </div>
---
+++ src/helpers/countryList.js
@@ -0,0 +1,13 @@
+const cities = [
+    {
+        city: "Szczecin",
+        cordinates: "lat=53.429&lon=14.553",
+        Value: "Szczecin, szcecin, Щецин, щецин"
+    },
+    {
+        city: "Warszawa",
+        cordinates: "lat=52.2298&lon=21.0118",
+        Value: "Warszawa, warszawa, Варшава, варшава"
+    }
+]
+export {cities}
